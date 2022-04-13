ae71d74033015c45921ea44c0d0b13c1caf10f16
headiks <head3ker@yandex.ru>
2022-04-13 19:33:55 +0300

    добавлены основные функции бота и код на ардуино

---
+++ main.py
@@ -0,0 +1,126 @@
+import io
+import os
+import time
+from peewee import *
+import cv2
+import serial
+import telebot
+from keyboa import Keyboa
+
+bot = telebot.TeleBot('5180282626:AAEDq-6h8OxZkX55tbPkMvgMPMX91AMAajc')
+ser = serial.Serial('COM11', baudrate=9600, timeout=1)
+ser.flush()
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+sio.flush()
+udid = 5120633740
+conn = SqliteDatabase('project.db')
+cursor = conn.cursor()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = conn
+
+
+class date(BaseModel):
+    artist_id = AutoField(column_name='Date')
+    name = TextField(column_name='photo_name', null=True)
+
+    class Meta:
+        table_name = 'photodata'
+
+
+@bot.message_handler(content_types=['text', 'document', 'photo'])
+def message(message):
+    print(message.chat.id)
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(3, 800)  # set Width
+    cap.set(4, 480)  # set Height
+    pasttime = 0
+    facephoto = 0
+    buttonswithids = [
+        {"вывести данные": "1"}]
+    global kb
+    kb = Keyboa(items=buttonswithids).keyboard
+    door = [
+        {"Да": "3"}, {"Нет": "4"}]
+    doorkb = Keyboa(items=door).keyboard
+    bot.send_message(chat_id=message.chat.id, reply_markup=kb, text="Что вам нужно")
+    while True:
+        thistime = time.time()
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(20, 20)
+        )
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = img[y:y + h, x:x + w]
+            if int(thistime) - pasttime > 10:
+                pasttime = thistime
+                return_value, img = cap.read()
+                times = time.asctime().split(' ')
+                times2 = times[3]
+                times.pop(3)
+                times = times2.split(':') + times
+                times = '_'.join(times)
+                global path
+                path = 'D:/python_photos/'
+                global fpath
+                fpath = f'{str(times)}.jpg'
+                cv2.imwrite(os.path.join(path, fpath), img)
+                facephoto = 1
+        if facephoto == 1:
+            bot.send_photo(chat_id=udid, photo=open(path + fpath, 'rb'))
+            bot.send_message(chat_id=udid, reply_markup=doorkb, text="Открыть дверь?")
+            print(path + fpath)
+            date.create(
+                artist_id=f'{time.gmtime(1575721830).tm_year}-{time.gmtime(1575721830).tm_mon}-{time.gmtime(1575721830).tm_mday}',
+                name=f'{path + fpath}')
+            facephoto = 0
+        cv2.imshow('video', img)
+        if message.text == 'обработка':
+            try:
+                photo_id = message.photo[-1].file_id
+                bot.send_message(message.chat.id, 'получил фото')
+            except:
+                bot.send_message(message.chat.id, 'получил что угодно, но определенно не фото')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    if call.data == '1':
+        senddata(1)
+    elif call.data == '2':
+        senddata(2)
+    elif call.data == '3':
+        senddata(3)
+    elif call.data == '4':
+        senddata(4)
+
+
+def senddata(number):
+    if number == 1:
+        ser.write(b'1')
+        line = ser.readline().decode('ASCII').split('-')
+        bot.send_message(chat_id=udid, text=f'''
+    Температура = {line[0]}
+    Влажность   = {line[1]}
+    ''')
+    elif number == 2:
+        raise SystemExit
+    elif number == 3:
+        ser.write(b'2')
+        bot.send_message(udid, text='Дверь открыта')
+        bot.send_message(chat_id=udid, reply_markup=kb, text="Что вам нужно")
+    elif number == 4:
+        bot.send_message(udid, text='Полиция вызвана,дверь Заблокирована')
+        bot.send_message(chat_id=udid, reply_markup=kb, text="Что вам нужно")
+
+
+bot.polling(none_stop=True, interval=0)
---
+++ umnii_dom/umnii_dom.ino
@@ -0,0 +1,250 @@
+#define AMOUNT 1  // кол-во серво
+#include "ServoDriverSmooth.h"
+#include <ServoSmooth.h>
+ServoSmooth servos[AMOUNT];
+#include "TM1637.h"
+#include <Wire.h>
+#include <dht11.h>  // Подключение библиотеки
+dht11 DHT;          // Создаем объект
+#define DHT11_PIN 10  // Определяем pin к которому подключен датчик
+#include <iarduino_RTC.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int8_t DispMSG[4] {1, 2, 3, 4};
+#define CLK 3
+#define DIO 2
+TM1637 tm1637(CLK, DIO);
+iarduino_RTC watch(RTC_DS3231);
+#include <NewPing.h>
+
+#define TRIGGER_PIN  13
+#define ECHO_PIN     12
+#define MAX_DISTANCE 400
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+int sw = 1 ;
+int tl, i;
+int tm, adb;
+int x, xv, xmp;
+int hour1;
+int hour2;
+int hour3 = 22;
+int minute1;
+int minute2;
+int minute3 = 55;
+int butTime = 7;
+int plus = 6;
+int minus = 5;
+int date = 4;
+int temp, hum, chk;
+int timess;
+int axy, guy, distguy, svetok;
+int svett;
+byte bukva_ZH[8]  = {B10101, B10101, B10101, B11111, B10101, B10101, B10101, B00000,}; // Буква "Ж"
+byte bukva_N[8]   = {B10001, B10001, B10001, B11111, B10001, B10001, B10001, B00000,}; // Буква "Н"
+byte bukva_L[8]   = {B00011, B00111, B00101, B00101, B01101, B01001, B11001, B00000,}; // Буква "Л"
+byte bukva_P[8]   = {B11111, B10001, B10001, B10001, B10001, B10001, B10001, B00000,}; // Буква "П"
+byte bukva_Mz[8]  = {B10000, B10000, B10000, B11110, B10001, B10001, B11110, B00000,}; // Буква "Ь"
+byte bukva_t[8]   = {B00000, B00000, B11111, B10101, B00100, B00100, B00100, B00000,}; // Буква "т"
+#define PIN_LED 7
+void setup() {
+  pinMode(A0, INPUT);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, HIGH);
+  delay(300);
+  tm1637.init();
+  tm1637.set(BRIGHTEST);
+  Serial.begin(9600);
+  watch.begin();
+  tl = millis();
+  tm = millis();
+  lcd.init();// Инициализация дисплея
+  lcd.backlight();// Включаем подсветку дисплея
+  lcd.createChar(1, bukva_P);// Создаем символ под номером 1
+  lcd.createChar(2, bukva_L);// Создаем символ под номером 1
+  lcd.createChar(3, bukva_ZH);// Создаем символ под номером 8
+  lcd.createChar(4, bukva_N);// Создаем символ под номером 4
+  lcd.createChar(6, bukva_t);
+  lcd.createChar(7, bukva_Mz);// Создаем символ под номером 2
+  servos[0].attach(4, 40);
+  servos[0].setAccel(0);
+  servos[0].setSpeed(800);
+  watch.settime(50 , 59, 13, 21, 8, 2021, 5);
+}
+void loop() {
+  switch (sw) {
+    case 1:
+      temp = DHT.temperature;  // Читаем температуру
+      hum = DHT.humidity;
+      tm1637.point(true);
+      hour1 = watch.Hours;
+      hour2 = watch.Hours;
+      minute1 = watch.minutes;
+      minute2 = watch.minutes;
+      hour1 = hour1 / 10;
+      hour2 = hour2 % 10;
+      minute1 = minute1 / 10;
+      minute2 = minute2 % 10;
+      DispMSG[0] = hour1;
+      DispMSG[1] = hour2;
+      DispMSG[2] = minute1;
+      DispMSG[3] = minute2;
+      tm1637.display(DispMSG);
+      butTime =  digitalRead(7);
+      if (butTime == 0) {
+        sw = 2;
+      }
+      if (hour1 == 2)
+        digitalWrite(12, HIGH);
+      if (hour1 == 0 and hour2 > 6)
+        digitalWrite(11, HIGH);
+      break;
+    case 2:
+      plus = digitalRead(6);
+      minus = digitalRead(5);
+      if (plus == 1) {
+        x++;
+        DispMSG[0] =  x;
+      } if (minus == 1) {
+        x--;
+        DispMSG[0] = x;
+      }
+      if (x >= 3 || x <= -1) {
+        x = 0;
+        DispMSG[0] = x;
+      }
+      tm1637.display(DispMSG);
+      butTime =  digitalRead(7);
+      if (butTime == 0) {
+        watch.settime(0, minute3, hour3);
+        sw = 3;
+      }
+      break;
+    case 3:
+      plus = digitalRead(6);
+      minus = digitalRead(5);
+      if (plus == 1) {
+        x++;
+        DispMSG[1] =  x;
+      } if (minus == 1) {
+        x--;
+        DispMSG[1] = x;
+      }
+      if (x >= 10 || x <= -1) {
+        x = 0;
+        DispMSG[1] = x;
+      }
+      tm1637.display(DispMSG);
+      butTime =  digitalRead(7);
+      if (butTime == 0) {
+        watch.settime(0, minute3, hour3);
+        sw = 4;
+      }
+      break;
+    case 4:
+      plus = digitalRead(6);
+      minus = digitalRead(5);
+      if (plus == 1) {
+        x++;
+        DispMSG[2] =  x;
+      } if (minus == 1) {
+        x--;
+        DispMSG[2] = x;
+      }
+      if (x >= 6 || x <= -1) {
+        x = 0;
+        DispMSG[2] = x;
+      }
+      tm1637.display(DispMSG);
+      butTime =  digitalRead(7);
+      if (butTime == 0) {
+        watch.settime(0, minute3, hour3);
+        sw = 5;
+      }
+      break;
+    case 5:
+      plus = digitalRead(6);
+      minus = digitalRead(5);
+      if (plus == 1) {
+        x++;
+        DispMSG[3] =  x;
+      } if (minus == 1) {
+        x--;
+        DispMSG[3] = x;
+      }
+      if (x >= 10 || x <= -1) {
+        x = 0;
+        DispMSG[3] = x;
+      }
+      tm1637.display(DispMSG);
+      butTime =  digitalRead(7);
+      if (butTime == 0) {
+        hour3 =   DispMSG[0] * 10 + DispMSG[1];
+        minute3 =   DispMSG[2] * 10 + DispMSG[3];
+        watch.settime(0, minute3, hour3);
+        sw = 1;
+      }
+      break;
+  }
+  switch (xv) {
+    case 0:
+      chk = DHT.read(DHT11_PIN);
+      temp = DHT.temperature;  // Читаем температуру
+      hum = DHT.humidity;      // Читаем влажность воздуха
+      tl = millis();
+      axy = Serial.read();
+      lcd.setCursor(0, 0);             // Установка курсора в начало второй строки
+      lcd.print("Tem\1epa\6. =");
+      lcd.print(temp);
+      lcd.setCursor(0, 1);             // Установка курсора в начало второй строки
+      lcd.print("B\2a\3\4oc\6\7 =");
+      lcd.print(hum);
+      if (axy == 49) {
+        Serial.print(temp);
+        Serial.print("-");
+        Serial.print(hum);
+        Serial.println("-");
+        
+      }
+      if (axy == 50) {
+
+        timess = millis();
+        guy = 1;
+      }
+      break;
+  }
+  switch (guy) {
+    case  0:
+      servos[0].tick();
+      servos[0].setTargetDeg(179);
+      break;
+    case 1:
+      servos[0].tick();
+      servos[0].setTargetDeg(80);
+      distguy = sonar.ping_cm();
+      if (distguy > 50) {
+        if ((millis() - timess) > 5000) {
+          guy = 0;
+        }
+      } else timess = millis();
+      break;
+  }
+  switch (svetok) {
+    case  0:
+      analogWrite(PIN_LED, LOW);
+      svett = analogRead(A0);
+      if (svett < 100) {
+        svetok = 1;
+      }
+      break;
+    case  1:
+      svett = analogRead(A0);
+        digitalWrite(PIN_LED, HIGH);
+      if (svett > 100) {
+        svetok = 0;
+      }
+      break;
+  }
+}
